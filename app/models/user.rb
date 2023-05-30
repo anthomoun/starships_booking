a1529a780ff2ecc0e28090c10aaef8464cc6de9e
Julien Pons <jp93rom@orange.fr>
2023-05-30 14:21:48 +0200

    Add dependence destruction on users and starships models.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :starships
-  has_many :bookings
+  has_many :starships, dependent: :destroy
+  has_many :bookings, dependent: :destroy
 end
