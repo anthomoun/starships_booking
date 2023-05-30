ffddd08b529b36f94ab049f4b050e8ac40509da7
Anthony Christodoulou <christodoulou75@gmail.com>
2023-05-30 14:27:45 +0200

    Navbar design done, need to add menu dropdown

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-ActiveRecord::Schema[7.0].define(version: 2023_05_29_130859) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-=======
-ActiveRecord::Schema[7.0].define(version: 2023_05_29_151008) do
-=======
 ActiveRecord::Schema[7.0].define(version: 2023_05_30_110538) do
->>>>>>> master
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,7 +26,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_30_110538) do
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
->>>>>>> master
   create_table "starships", force: :cascade do |t|
     t.string "name"
     t.string "model"
@@ -67,17 +56,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_30_110538) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
   add_foreign_key "bookings", "starships"
   add_foreign_key "bookings", "users"
-
->>>>>>> master
-=======
-  add_foreign_key "bookings", "starships"
-  add_foreign_key "bookings", "users"
->>>>>>> master
   add_foreign_key "starships", "users"
 end
