4669fc5698478687005ce9426979fd4bf73b9bb7
nicolas bourdin <nicolas_bourdin@hotmail.com>
2023-05-30 12:10:04 +0200

    ajout de starships par api seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,26 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+require "open-uri"
+require 'json'
+Starship.destroy_all
+
+ADDRESS = %w[Cayenne Marseille Washington Phuket].freeze
+url = "https://swapi.dev/api/starships.format=json"
+
+starships_serialized = URI.open(url).read
+starships = JSON.parse(starships_serialized)["results"]
+
+starships.each do |starship|
+  Starship.create!(
+    name: starship['name'],
+    type: starship['model'],
+    number_of_persons: starship['passengers'],
+    address: ADDRESS.sample,
+    description: "#{starship['starship_class']}, manufactured by #{starship['manufacturer']}",
+    price: starship['cost_in_credits'],
+    poster_url: "https://picsum.photos/200/300"
+  )
+end
+
+puts "created #{Starship.count} starships !"
