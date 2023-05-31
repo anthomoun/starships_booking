13c9e67f46fc54c24c26b914c923e2cff60df912
Julien Pons <jp93rom@orange.fr>
2023-05-31 14:14:46 +0200

    Change column model in seed, decomment my photos code that was commented, reset root as homepage rather than starships.

--- app/models/starship.rb
+++ app/models/starship.rb
@@ -1,9 +1,9 @@
 class Starship < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
-  # has_many_attached :photos
+  has_many_attached :photos
   validates :name, :number_of_persons, :address, :price, :description, presence: true
- 
+
 
   def unavailable_dates
     bookings.pluck(:start_date, :end_date).map do |range|
