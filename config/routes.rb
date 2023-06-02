533cff02b857eec2c5934f3fd5b3f5ab37d3c7fc
Maxence Tarnaud <maxence.trnd@gmail.com>
2023-06-02 16:34:05 +0200

    fix dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -14,10 +14,9 @@ Rails.application.routes.draw do
 
   resources :bookings, only: [ :show, :destroy ]
   get 'accept_booking/:id', to: 'dashboard#accept_booking', as: 'accepted_booking'
-  get 'refuse_booking', to: 'dashboard#refuse_booking', as: 'refused_booking'
+  get 'refuse_booking/:id', to: 'dashboard#refuse_booking', as: 'refused_booking'
 
   get 'dashboard', to: 'dashboard#index'
-  patch 'dashboard/bookings/:id/update_status', to: 'dashboard#update_booking_status', as: 'update_booking_status'
 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
